71e46e058b71359bbbe04419b1f85ba38846655e
ZeroNine <fajarnet1999@gmail.com>
2021-12-07 00:29:36 -0800

    fix error admin/user

--- routes/web.php
+++ routes/web.php
@@ -32,34 +32,40 @@ Route::group(['prefix'=> 'admin', 'middleware'=>['admin:admin']], function(){
     Route::post('/login',[AdminController::class, 'store'])->name('admin.login');
 });
 
-
+Route::middleware(['auth:admin'])->group(function(){
 Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
     return view('admin.index');
 })->name('admin.dashboard');
 
 //admin al route
-Route::get('/admin/logout',[AdminController::class, 'destroy'])->name('admin.logout'); 
+Route::get('/admin/logout',[AdminController::class, 'destroy'])->name('admin.logout');; 
 
-Route::get('/admin/profile',[AdminProfileController::class, 'adminProfile'])->name('admin.profile'); 
-Route::get('/admin/profile/edit',[AdminProfileController::class, 'profileEdit'])->name('admin.profile.edit'); 
-Route::post('/admin/profile/update',[AdminProfileController::class, 'profileUpdate'])->name('admin.profile.update'); 
-Route::get('/admin/change/password',[AdminProfileController::class, 'changePassword'])->name('admin.change.password'); 
-Route::post('/admin/update/password',[AdminProfileController::class, 'passwordUpdate'])->name('admin.password.update'); 
+Route::get('/admin/profile',[AdminProfileController::class, 'adminProfile'])->name('admin.profile');; 
+Route::get('/admin/profile/edit',[AdminProfileController::class, 'profileEdit'])->name('admin.profile.edit');; 
+Route::post('/admin/profile/update',[AdminProfileController::class, 'profileUpdate'])->name('admin.profile.update');; 
+Route::get('/admin/change/password',[AdminProfileController::class, 'changePassword'])->name('admin.change.password');; 
+Route::post('/admin/update/password',[AdminProfileController::class, 'passwordUpdate'])->name('admin.password.update');; 
 
+}); // end Middleware admin
 
+Route::get('/',[IndexController::class, 'index'])->name('home'); 
 //user all route
+Route::middleware(['auth:web'])->group(function(){
 
 Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
     $user = User::find(Auth::user()->id);
     return view('dashboard', compact('user'));
 })->name('dashboard');
 
-Route::get('/',[IndexController::class, 'index'])->name('home'); 
+
 Route::get('/user/logout',[IndexController::class, 'userLogout'])->name('user.logout'); 
 Route::get('/user/profile',[IndexController::class, 'userProfile'])->name('user.profile'); 
 Route::post('/user/profile/update',[IndexController::class, 'userUpdate'])->name('user.profile.update'); 
 Route::get('/user/change/password',[IndexController::class, 'userPass'])->name('user.change.password'); 
 Route::post('/user/update/password',[IndexController::class, 'passwordUpdate'])->name('user.password.update'); 
+
+}); // end Middleware user
+
 
 //admin brand route
 Route::prefix('brand')->group(function(){
