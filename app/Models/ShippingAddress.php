2004eaea057693ced3ff1c13d36e5c6c6471d090
ZeroNine <fajarnet1999@gmail.com>
2022-11-10 19:26:22 +0700

    10/11/22 2

--- app/Models/ShippingAddress.php
+++ app/Models/ShippingAddress.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ShippingAddress extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
--- database/migrations/2022_11_08_133308_create_shipping_addresses_table.php
+++ database/migrations/2022_11_08_133308_create_shipping_addresses_table.php
@@ -15,6 +15,14 @@ class CreateShippingAddressesTable extends Migration
     {
         Schema::create('shipping_addresses', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('division_id');
+            $table->unsignedBigInteger('district_id');
+            $table->unsignedBigInteger('state_id');
+            $table->string('name');
+            $table->string('email');
+            $table->integer('code_post');
+            $table->text('notes');
             $table->timestamps();
         });
     }
